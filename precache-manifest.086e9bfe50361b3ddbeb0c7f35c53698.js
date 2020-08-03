d06c23b5c633cae8f2363bdf9529303d1d75f21c
ganha <milo.somera@gmail.com>
2020-08-03 11:45:04 +0800

    Updates

---
+++ precache-manifest.086e9bfe50361b3ddbeb0c7f35c53698.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1737e5dc01086d8b21bdffd64f8da1d",
+    "url": "/pelicula/index.html"
+  },
+  {
+    "revision": "146bd00f9d7b434dd827",
+    "url": "/pelicula/static/css/main.00f64fff.chunk.css"
+  },
+  {
+    "revision": "7d9eb47edd51f81fba79",
+    "url": "/pelicula/static/js/2.6b088ece.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/pelicula/static/js/2.6b088ece.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "146bd00f9d7b434dd827",
+    "url": "/pelicula/static/js/main.b9a4d250.chunk.js"
+  },
+  {
+    "revision": "cebaf1fea48fd46c96a5",
+    "url": "/pelicula/static/js/runtime-main.992052af.js"
+  }
+]);
